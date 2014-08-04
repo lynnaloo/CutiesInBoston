9f742d025b3922c485dc897d2c5035ff2105ce19
Linda Nichols <lynnaloo@gmail.com>
2014-08-04 04:41:32 +0000

    fixed formatting

--- lib/cuties/twit.rb
+++ lib/cuties/twit.rb
@@ -25,9 +25,9 @@ class Twit
   def client
     Twitter::REST::Client.new do |config|
       begin
-        config.consumer_key =        ENV['consumer_key']
-        config.consumer_secret =     ENV['consumer_secret']
-        config.access_token =        ENV['access_token']
+        config.consumer_key        = ENV['consumer_key']
+        config.consumer_secret     = ENV['consumer_secret']
+        config.access_token        = ENV['access_token']
         config.access_token_secret = ENV['access_token_secret']
 
       rescue KeyError
